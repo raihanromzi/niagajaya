7435dc8e3c698c685aa5bca314003d9aae028ca0
jason@karjadi.com <jason@karjadi.com>
2023-03-12 02:55:49 +0700

    add new form components

--- projects/client/src/components/StyledInput.jsx
+++ projects/client/src/components/StyledInput.jsx
@@ -1,6 +1,6 @@
 import { Input } from "@chakra-ui/react";
 
-/** @param {import("react").InputHTMLAttributes<HTMLInputElement>} props */
+/** @param {import("react").InputHTMLAttributes<HTMLInputElement> & import("@chakra-ui/react").InputProps} props */
 const StyledInput = (props) => {
   return (
     <Input bgColor="#F1FBF8" borderRadius="4px" variant="filled" {...props} />
