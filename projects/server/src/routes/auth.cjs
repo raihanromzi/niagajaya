30a54c76478865a774e94f84cab9d8e6a68a6a7d
jason@karjadi.com <jason@karjadi.com>
2023-03-19 17:43:05 +0700

    update validations and routes

--- projects/server/src/routes/auth.cjs
+++ projects/server/src/routes/auth.cjs
@@ -6,16 +6,15 @@ const {
   check,
   login,
 } = require("../controllers/auth.cjs");
-const {
-  registerSchema,
-  setPasswordSchema,
-} = require("../validations/auth.cjs");
+const emailSchema = require("../validations/email.cjs");
+const nameSchema = require("../validations/name.cjs");
+const passwordSchema = require("../validations/password.cjs");
 
 const { validate, loginValidator } = require("../validations/login.cjs");
 
 router
-  .post("/register", checkSchema(registerSchema), register)
-  .post("/set-password", checkSchema(setPasswordSchema), setPassword);
+  .post("/register", checkSchema({ ...emailSchema, ...nameSchema }), register)
+  .post("/set-password", checkSchema({ ...passwordSchema }), setPassword);
 
 router.post("/v2", loginValidator(), validate, login);
 router.get("/v3", check);
