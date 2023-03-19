cd1c2ac6d74470b5a062883ccb4d19de9e0cc537
jason@karjadi.com <jason@karjadi.com>
2023-03-19 17:47:40 +0700

    add and update components

--- projects/client/src/components/ProfileItem.jsx
+++ projects/client/src/components/ProfileItem.jsx
@@ -1,14 +1,16 @@
 import { Box, Flex, FormLabel, IconButton, Input } from "@chakra-ui/react";
 import { FaEdit } from "react-icons/fa";
 
-const ProfileItem = ({ id, label, value, handleClick }) => {
+const ProfileItem = ({ id, label, defaultValue, handleClick }) => {
   return (
     <Box w="full">
-      <FormLabel htmlFor={id}>{label}</FormLabel>
+      <FormLabel htmlFor={id} m="0">
+        {label}
+      </FormLabel>
       <Flex>
         <Input
           id={id}
-          value={value}
+          defaultValue={defaultValue}
           variant="filled"
           borderRightRadius="none"
           isDisabled
