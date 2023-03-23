be7dfc1f4c974bf2256034d4442ee151f72d33b9
Raihan Romzi <raihan.rakhman16@gmail.com>
2023-03-23 10:30:40 +0700

    add validator email

--- projects/server/src/routes/admin/userRouter.js
+++ projects/server/src/routes/admin/userRouter.js
@@ -3,15 +3,17 @@ const router = express.Router();
 
 const userContoller = require("../../controllers/admin/user/userController");
 
+import { emailValidator } from "../../validations/admin/loginValidation";
+
 // Get All Admin
 // localhost:8000/api/admin?role=User -> Must add req query
 router.get("/admin", userContoller.getAllUsers);
 
 // Create Admin
-router.post("/admins", userContoller.createAdmin);
+router.post("/admins", emailValidator(), userContoller.createAdmin);
 
 // Update Admin
-router.put("/admin/:adminID", userContoller.updateAdmin);
+router.put("/admin/:adminID", emailValidator(), userContoller.updateAdmin);
 
 // delete Admin
 router.delete("/admin/:adminID", userContoller.deleteAdmin);
