47a2b8ec2a5b3f7ebce3285b067c2911b235ff17
Raihan Romzi <raihan.rakhman16@gmail.com>
2023-05-22 20:12:43 +0700

    fix bug

--- projects/client/src/components/AdminLayout.jsx
+++ projects/client/src/components/AdminLayout.jsx
@@ -13,50 +13,53 @@ import { FaHome } from "react-icons/fa";
 import { Link as RouterLink, NavLink, Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import PageProtected from "../routes/protected";
 
 const AdminLayout = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth);
 
   return (
-    <Flex bgColor="#F5F7FA" h="100vh">
-      <Flex
-        flexDir="column"
-        justifyContent="space-between"
-        p={4}
-        bgColor="#FCFCFC"
-        borderRight="solid 1px #E7E7E7"
-        w={60}>
-        <VStack spacing={5} color="#717171">
-          <Heading
-            as={RouterLink}
-            to="/admin"
-            fontSize="xl"
-            color="#009262"
-            py={10}>
-            Niagajaya
-          </Heading>
-          <Heading w="full" fontSize="sm" fontWeight="medium">
-            MENU
-          </Heading>
-          <Button as={NavLink} to="/admin" variant="ghost" w="full" pr={2}>
-            <Icon as={FaHome} mr={2.5} />
-            <Text as="span" w="full">
-              Beranda
-            </Text>
-          </Button>
-          <Divider />
-        </VStack>
-        <Flex>
-          <Avatar borderRadius="10px" />
-          <Box>
-            <Heading>Shimarin</Heading>
-            <Text>Manager</Text>
-          </Box>
+    <PageProtected needLogin={true}>
+      <Flex bgColor="#F5F7FA" h="100vh">
+        <Flex
+          flexDir="column"
+          justifyContent="space-between"
+          p={4}
+          bgColor="#FCFCFC"
+          borderRight="solid 1px #E7E7E7"
+          w={60}>
+          <VStack spacing={5} color="#717171">
+            <Heading
+              as={RouterLink}
+              to="/admin"
+              fontSize="xl"
+              color="#009262"
+              py={10}>
+              Niagajaya
+            </Heading>
+            <Heading w="full" fontSize="sm" fontWeight="medium">
+              MENU
+            </Heading>
+            <Button as={NavLink} to="/admin" variant="ghost" w="full" pr={2}>
+              <Icon as={FaHome} mr={2.5} />
+              <Text as="span" w="full">
+                Beranda
+              </Text>
+            </Button>
+            <Divider />
+          </VStack>
+          <Flex>
+            <Avatar borderRadius="10px" />
+            <Box>
+              <Heading>Shimarin</Heading>
+              <Text>Manager</Text>
+            </Box>
+          </Flex>
         </Flex>
+        <Outlet />
       </Flex>
-      <Outlet />
-    </Flex>
+    </PageProtected>
   );
 };
 
